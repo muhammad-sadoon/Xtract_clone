3a3028ceff93ad795db7fd160d3a134cea4bf91e
sadoon <sadoonmuhammad8@gmail.com>
2025-06-05 20:55:38 +0500

    add items

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
+AOS.init();
 let closeBtn = document.querySelector(".closeBtn");
 let navbar = document.querySelector(".navbar");
 flage = true
-closeBtn.addEventListener("click",()=>{
-    if(flage){
+closeBtn.addEventListener("click", () => {
+    if (flage) {
         navbar.style.opacity = 1;
         flage = false
-    }else{
+    } else {
         navbar.style.opacity = 0;
         flage = true
     }
 })
+
+let li_items = document.querySelectorAll(".page_1 ul li");
+let ul = document.querySelector(".page_1 ul")
+let arr = [];
+let animateBoolean = false
+li_items.forEach((v) => {
+    arr.push(v)
+})
+
+
+function animateMove() {
+    var item = arr.shift()
+    ul.removeChild(item)
+    ul.appendChild(item)
+    arr.push(item)
+
+}
+var setItervalAnimationFrame = setInterval(animateMove, 2000)
